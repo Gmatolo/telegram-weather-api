cc0831c9f77a173bc9245b630f6c0f071566870e
gmatolo <geraldmuli7@gmail.com>
2022-07-30 11:41:10 +0300

    imports dependencies

--- telegram.py
+++ telegram.py
@@ -1,2 +1,6 @@
-api_key = "teleg-api-weatherbot"
-
+from telegram.ext.updater import Updater
+from telegram.update import Update
+from telegram.ext.callbackcontext import CallbackContext
+from telegram.ext.commandhandler import CommandHandler
+from telegram.ext.messagehandler import MessageHandler
+from telegram.ext.filters import Filters
